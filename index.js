5289b4a884c33cbe15b7cac381be33ec53076e24
Mateus <creztfallen@M.S.M>
2022-05-03 19:23:58 -0300

    changing post method with fetch api

--- index.js
+++ index.js
@@ -60,7 +60,6 @@ MongoClient.connect(MONGOURL)
     });
 
     app.post("/dices", (req, res) => {
-      console.log(req.body);
       const playerName = req.body.playerName;
       const diceType = req.body.diceType;
       const diceValue = randomIntFromInterval(1, diceType);
@@ -71,8 +70,7 @@ MongoClient.connect(MONGOURL)
           diceValue: diceValue,
         })
         .then((result) => {
-          // res.json({ roll: diceValue });
-          res.redirect("/");
+          res.json({ roll: diceValue });
         })
         .catch((error) => console.error(error));
     });
